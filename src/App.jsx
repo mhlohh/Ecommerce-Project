30cef3dc65edccbfdbc2d2a9cc2ad53395d99dc8
Muhsil NR <rockriderskerala@gmail.com>
2026-05-27 14:23:51 +0530

    Update Links to Match router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import  {HomePage}  from './pages/HomePage.jsx';
 import {Routes,Route} from 'react-router';
 import { CheckoutPage } from './pages/CheckoutPage.jsx';
+import {OrderPage} from './pages/OrderPage.jsx';
 import './App.css';
 export function App() {
   return(
     <Routes>
       <Route index element={<HomePage/>} />
       <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='orders' element={<OrderPage />}/>
     </Routes>
   );
 }
