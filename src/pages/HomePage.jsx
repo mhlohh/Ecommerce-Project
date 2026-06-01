d5858f5ffaeefc62c8a740344313cde8c44dc2b2
Muhsil NR <rockriderskerala@gmail.com>
2026-06-01 10:27:18 +0530

    generate the Html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import { Link } from 'react-router'
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data)
+        })
     return (
         <>
             <title>Ecommerce Project</title>
